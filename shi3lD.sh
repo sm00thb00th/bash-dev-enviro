b932e0fd70f9d04348d8f594c40bf5705e7c15cc
sm00thb00th <donko.ivo.gl.ser@gmail.com>
2017-09-01 00:56:32 +0200

    shi3lD.sh

--- shi3lD.sh
+++ shi3lD.sh
@@ -27,56 +27,56 @@ puffed='' ;
 nnumberr="0" ;
 #
 puffeRR(){
-            if [[ "$(netstat -ar)" =~ 'default' ]] ;
-    then
+		if [[ "$(netstat -ar)" =~ 'default' ]] ;
+	then
 		. /usr/local/bin/stop_shield.sh ;
-    else
-        clear;
+	else
+		clear;
 		echo -e "\n ::    D A N G E R      ::" ;
-        echo -e "\n :: your shield is down :: \n" && . /usr/local/bin/start_shield.sh ;
-    fi
+		echo -e "\n :: your shield is down :: \n" && . /usr/local/bin/start_shield.sh ;
+	fi
 }
 
 exitHandler(){
 
-        echo -e "-"
-        printf " quit? (y/n) " ; read
-        echo -e "-"
+		echo -e "-"
+		printf " quit? (y/n) " ; read
+		echo -e "-"
 
-            if [[ $REPLY =~ y|Y|j|J ]] ;
-        then
+			if [[ $REPLY =~ y|Y|j|J ]] ;
+		then
 			let df4="$nnumberr"/10/60 ;
-            sudo ip link set dev "$interface" down && sleep 5 && 
-            sudo ip link set dev "$interface" up && 
-            sudo ip link set dev "$interface" address "$macadd" && 
-            sudo systemctl restart snort.service && clear && 
+			sudo ip link set dev "$interface" down && sleep 5 && 
+			sudo ip link set dev "$interface" up && 
+			sudo ip link set dev "$interface" address "$macadd" && 
+			sudo systemctl restart snort.service && clear && 
 			echo -e "\n .You where surfing with this MAC:\n\n
 			$(find /home/dob/* -name "*mac_recieves_dhcp_lease*" | grep "$(date | \
 			awk '{print $2,$6}' | sed 's/\ //g')" | xargs cat | tail -n1)\n" ;
 			exit 0 ;
 
-            elif [[ $REPLY =~ n|N ]] ;
-        then
-            return ;
-    fi
+			elif [[ $REPLY =~ n|N ]] ;
+		then
+			return ;
+	fi
 }
-            while trap 'exitHandler' SIGINT ;
+		while trap 'exitHandler' SIGINT ;
 	do
-            if [[ "$(ps aux | grep -E -i -w 'snort|clamd' | grep -v grep | awk '{print $11}' | grep -E 'snort|clamd' | \
-            wc -l | tr -d ' ')" = "2" ]] && [[ "$(ip link show "$interface" | grep ether | awk '{print $2}')" != "$macadd" ]] ;
+		if [[ "$(ps aux | grep -E -i -w 'snort|clamd' | grep -v grep | awk '{print $11}' | grep -E 'snort|clamd' | \
+		wc -l | tr -d ' ')" = "2" ]] && [[ "$(ip link show "$interface" | grep ether | awk '{print $2}')" != "$macadd" ]] ;
 	then
-            spin='.oOo' ; i=0 ;
-            spin2='-\|/' ;
+		spin='.oOo' ; i=0 ;
+		spin2='-\|/' ;
 
-            while [[ "$(ps aux | grep -E -i -w 'snort|clamd' | grep -v grep | awk '{print $11}' | grep -E 'snort|clamd' | \
-            wc -l | tr -d ' ')" = "2" ]] && [[ "$(ip link show "$interface" | grep ether | awk '{print $2}')" != "$macadd" ]] ;
-        do
-            ((nnumberr++));
-            i=$(( (i+1) %4 )) ; printf "\r ${spin2:$i:1} UPTIME:$nnumberr${spin:$i:1} " ; sleep .1 ;
-        done
+		while [[ "$(ps aux | grep -E -i -w 'snort|clamd' | grep -v grep | awk '{print $11}' | grep -E 'snort|clamd' | \
+		wc -l | tr -d ' ')" = "2" ]] && [[ "$(ip link show "$interface" | grep ether | awk '{print $2}')" != "$macadd" ]] ;
+	do
+		((nnumberr++));
+		i=$(( (i+1) %4 )) ; printf "\r ${spin2:$i:1} UPTIME:$nnumberr${spin:$i:1} " ; sleep .1 ;
+	done
 	else
-        puffeRR ;
-    fi
+		puffeRR ;
+	fi
 done
 
 fi
