8f1704e7afaf01417e29349ae3951a3f959c6caf
sm00thb00th <donko.ivo.gl.ser@gmail.com>
2017-09-01 18:44:58 +0200

    iNST4LL.sh

--- redundanz.sh
+++ redundanz.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # version 0.01
-# first run installs the environment
-# You can Add Programs to myPrograms, but remove bevor $HOME/installed
 # monitors the servers and applications, defined in server-monitor.sh
-
-myPrograms="xfce4-terminal figlet x11-apps imagemagick mc mutt eject nano snort clamav apache2 mysql-server pychecker shellcheck perl git" ;
 
 ###     WARNING:    DON'T EDIT ANYTHING BELOW       ###
 
@@ -23,59 +19,9 @@ ex1Th4ndler(){
 		wait ;
 		exit 0 ;
 }
-
-tester_(){
-
-nNuM=$(echo "$myPrograms" | wc -w) ;
-
-		while [[ "$nNuM" != "0" ]] ;
-	do
-		toInst=$(awk '{print $'"$nNuM"'}'<<<"$myPrograms") ;
-
-		if [[ "$toInst" = "imagemagick" ]] ;
-	then
-		toInst="display";
-
-		elif [[ "$toInst" = "x11-apps" ]] ;
-	then
-		toInst="transset";
-fi
-		wH3RE=$(whereis "$toInst" | cut -f2 -d: | cut -f2 -d\ ) ;
-# INSTALL
-		if [[ "${#wH3RE}" != "0" ]] ;
-	then
-		apPR="$toInst, allready installed" ;
-		((nNuM--)) ;
-	else
-		echo -e "\ninstalling ${toInst}..." ;
-		nohup apt-get --fix-missing --force-yes install "$toInst" | tee -a "/home/$SUDO_USER/installed" &
-		wait ;
-		apPR="$toInst, INSTALLED! " ;
-		sleep 1 ;
-		((nNuM--)) ;
-fi
-		if [ "$nNuM" = "0" ]
-	then
-		clear ;
-		echo -e "\n done." ;
-fi
-# ON THE SCREEN
-		printf "\rtesting for apps, $nNuM remaining, $apPR" ; sleep 2 ;
-		clear ;
-	done
-		chown "$SUDO_USER":"$SUDO_USER" "/home/$SUDO_USER/installed";
-
-		run_redundanz_;
-}
-
-run_redundanz_(){
-		setsid serv-if-up.sh >/dev/null 2>&1 < /dev/null &
-		watch -n3 -t --color server-monitor.sh ;
-}
 		if [ -e "/home/$SUDO_USER/installed" ] ;
 	then
-		run_redundanz_ ;
-	else
-		tester_ ;
+		setsid serv-if-up.sh >/dev/null 2>&1 < /dev/null &
+		watch -n3 -t --color server-monitor.sh ;
 	fi
 fi
