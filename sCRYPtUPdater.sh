3c9e8894af8d545a67547add0957af7afc7d167c
sm00thb00th <donko.ivo.gl.ser@gmail.com>
2017-09-01 18:26:20 +0200

    README.md

--- sCRYPtUPdater.sh
+++ sCRYPtUPdater.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # version 0.01
+
 # Set the time interval, and just hit enter to update, to stop "pychecker" or "perl -wc" hit ctrl+4
 #
 # Set the path to your Scripts like: dirINhome="myDistro" or dirINhome="Desktop/bash"
